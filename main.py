1c16d494e3efee342aa81bc9c0ac52e893c3bcd9
Nitish Raj Prince <84441583+nitishrajprince1@users.noreply.github.com>
2023-06-23 12:22:30 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,9 +4,9 @@ from utils import get_weather_data, into_xml_format, into_json_format
 def get_weather(city, output_format):
     weather_data = get_weather_data(city)
 
-    if output_format == 'xml':
+    if output_format == "xml":
         return into_xml_format(weather_data)
-    elif output_format == 'json':
+    elif output_format == "json":
         return into_json_format(weather_data)
     else:
-        print('use correct choice')
+        print("use correct choice")

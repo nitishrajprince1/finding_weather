2bbdac6331a551803ac96b10fc3d8ac28320a612
Nitish Raj Prince <84441583+nitishrajprince1@users.noreply.github.com>
2023-06-23 12:23:04 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -3,18 +3,16 @@ from xml.etree.ElementTree import Element, SubElement, tostring
 
 from constant import WEATHER_API_KEY
 
-URL = 'https://weatherapi-com.p.rapidapi.com/current.json'
+URL = "https://weatherapi-com.p.rapidapi.com/current.json"
 api_key = WEATHER_API_KEY
 
 
 def get_weather_data(city):
     my_headers = {
-        'X-RapidAPI-Key': api_key,
-        'X-RapidAPI-Host': 'weatherapi-com.p.rapidapi.com'
+        "X-RapidAPI-Key": api_key,
+        "X-RapidAPI-Host": "weatherapi-com.p.rapidapi.com",
     }
-    params = {
-        'q': city
-    }
+    params = {"q": city}
 
     try:
         response = requests.get(URL, headers=my_headers, params=params)
@@ -33,53 +31,54 @@ def get_weather_data(city):
 
 def into_json_format(weather_data):
     if weather_data:
-        weather = weather_data['current']
-        location = weather_data['location']
+        weather = weather_data["current"]
+        location = weather_data["location"]
         response = {
-            'City': f"{location['name']}, {location['country']}",
-            'Condition': weather['condition']['text'],
-            'Celsius': weather['temp_c'],
-            'Fahrenheit': weather['temp_f'],
-            'Wind': f"{weather['wind_mph']}mph, {weather['wind_kph']}kph",
-            'Humidity': weather['humidity'],
-            'Cloud': weather['cloud']
+            "City": f"{location['name']}, {location['country']}",
+            "Condition": weather["condition"]["text"],
+            "Celsius": weather["temp_c"],
+            "Fahrenheit": weather["temp_f"],
+            "Wind": f"{weather['wind_mph']}mph, {weather['wind_kph']}kph",
+            "Humidity": weather["humidity"],
+            "Cloud": weather["cloud"],
         }
 
     else:
-        response = {'error': 'Failed to fetch weather data'}
+        response = {"error": "Failed to fetch weather data"}
     return response
 
 
 def into_xml_format(weather_data):
     if weather_data:
-        weather = weather_data['current']
-        root = Element('root')
+        weather = weather_data["current"]
+        root = Element("root")
 
-        city = SubElement(root, 'City')
+        city = SubElement(root, "City")
         city.text = str(
-            weather_data['location']['name'] + ' ' + weather_data['location'][
-                'country'])
+            weather_data["location"]["name"] + " " + weather_data["location"]["country"]
+        )
 
-        condition = SubElement(root, 'Condition')
-        condition.text = str(weather['condition']['text'])
+        condition = SubElement(root, "Condition")
+        condition.text = str(weather["condition"]["text"])
 
-        temp_in_celsius = SubElement(root, 'Celsius')
-        temp_in_celsius.text = str(weather['temp_c'])
+        temp_in_celsius = SubElement(root, "Celsius")
+        temp_in_celsius.text = str(weather["temp_c"])
 
-        temp_in_fahrenheit = SubElement(root, 'Fahrenheit')
-        temp_in_fahrenheit.text = str(weather['temp_f'])
+        temp_in_fahrenheit = SubElement(root, "Fahrenheit")
+        temp_in_fahrenheit.text = str(weather["temp_f"])
 
-        wind = SubElement(root, 'Wind')
+        wind = SubElement(root, "Wind")
         wind.text = str(f"{weather['wind_mph']}mph, {weather['wind_kph']}kph")
 
-        humidity = SubElement(root, 'Humidity')
-        humidity.text = str(weather['humidity'])
+        humidity = SubElement(root, "Humidity")
+        humidity.text = str(weather["humidity"])
 
-        cloud = SubElement(root, 'Cloud')
-        cloud.text = str(weather['cloud'])
+        cloud = SubElement(root, "Cloud")
+        cloud.text = str(weather["cloud"])
 
-        xml_response = '<?xml version="1.0" encoding="UTF-8" ?>\n' + tostring(
-            root).decode()
+        xml_response = (
+            '<?xml version="1.0" encoding="UTF-8" ?>\n' + tostring(root).decode()
+        )
     else:
-        xml_response = '<error>Failed to fetch weather data</error>'
+        xml_response = "<error>Failed to fetch weather data</error>"
     return xml_response
